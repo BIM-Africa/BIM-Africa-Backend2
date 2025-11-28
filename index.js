f2bc8bf195f42462ee96a1b0adec99e2dd27d324
Hassan Omar <hassanomar7674@gmail.com>
2025-11-28 22:16:33 +0500

    updated

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ import { MongoClient } from 'mongodb'
 import allRoutes from './routes/index.js'
 import express from 'express'
 import cors from 'cors'
+import { connectDB } from './config/connectDB.js'
 
 const app = express()
 
 app.use(cors())
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+connectDB()
 // ✅ Simple root route
-app.get('/', (c) => c.text('✅ Server running on Cloudflare Workers!'))
+app.get('/', (req, res) => res.send('✅ Server running on Cloudflare Workers!'))
 
 app.use("/api", allRoutes)
 // CONTACT Endpoint
@@ -33,6 +36,9 @@ app.post("/api/contact", async (req, res) => {
   }
 });
 
+app.listen(5000, () => {
+  console.log('Server is running on port 3000');
+})
 
 // ✅ Export the app (Cloudflare entry point)
 export default app
